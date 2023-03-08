68303d795d8009139d719413278ec29639632418
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-08 10:50:57 +0100

    Initial commit

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+const controller = require("../controllers/users");
+const router = require("express").Router();
+
+//CRUD Routes /users
+router.get("/", controller.getUsers); //users
+router.get("/:userId", controller.getUsers); //users/:userId
+router.post("/", controller.createUser); // /users
+router.put("/:userId", controller.updateUser); // /users/:userId
+router.delete("/:userId", controller.deleteUser); // /users/:userId
