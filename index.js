b18afd6a41ed3a51473dec1c3d8db89f18380ea5
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-03-09 11:21:46 +0100

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const sequelize = require("./util/database");
-const User = require(".models.user");
+const User = require("./models/user");
 
 const app = express();
 
@@ -9,8 +9,8 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-    res.setHeader("Acces-Control-Allow-Origin", "*");
-    res.setHeader("Acces-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
     next();
 });
 
